daec86ec129ecbbfed32ec4b53200d6a25af565b
LockStockandalotofcode <shivanie0101@gmail.com>
2026-02-17 18:41:55 +0530

    final working solution with 3 sample inputs

--- business_game.py
+++ business_game.py
@@ -25,13 +25,19 @@ class BusinessGame:
 
             self.handle_turn_logic(curr_player, cell)
 
-            # maintaining Log for all turns
-            print(f"Turn: {turn_index + 1} Striker: {curr_player.name} got dice value: {dice_output} landed on cell type:{self.board.get_cell_at(new_pos)}" "\n" 
-                  f"\tMoved from {old_pos} to {new_pos}" )
+            # # for testing - maintaining Log for all turns
+            # print(f"Turn: {turn_index + 1} Striker: {curr_player.name} got dice value: {dice_output} landed on cell type:{self.board.get_cell_at(new_pos)}" "\n" 
+            #       f"\tMoved from {old_pos} to {new_pos}" )
             
-            for p in self.players:
-                print(f"{p.name} is at cell number : {p.position} and has {p.cash} money left while total worth is {p.net_worth}.")
-            print("-" * 10)
+            # for p in self.players:
+            #     print(f"{p.name} is at cell number : {p.position} and has {p.cash} money left while total worth is {p.net_worth}.")
+            # print("-" * 10)
+        
+    def display_winner(self):
+        # x is a player object in self.players list
+        self.players.sort(key=lambda x: x.net_worth, reverse=True)
+        for p in self.players:
+            print(f"{p.name} has total worth {p.net_worth}")
 
     def handle_turn_logic(self, player, cell):
         if isinstance(cell, Jail):
@@ -51,8 +57,27 @@ class BusinessGame:
 
 
 if __name__ == "__main__":
+    cells = "H,H,J,H,E,T,J,T,J,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,H,J,H,E,J,H,E,T,J,T,J,E,H,J,T,E,H,E"
+    dice = [1,2,4,1,7,8,5,11,10,12,2,3,5,6,7,8,5,11,10,12,2,3,5,6,7,8,5,11,10,12]
+    
+    game = BusinessGame(cells, dice, n_players=3)
+    game.run_game()
+    game.display_winner()
+    print("-" * 10)
+
+if __name__ == "__main__":
     cells = "E,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,H,J,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E0"
     dice = [4,4,4,6,7,8,5,11,10,12,2,3,5,6,7,8,5,11,10,12,2,3,5,6,7,8,5,11,10,12]
     
     game = BusinessGame(cells, dice, n_players=3)
     game.run_game()
+    game.display_winner()
+    print("-" * 10)
+
+if __name__ == "__main__":
+    cells = "J,H,J,H,E,T,J,T,J,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,H,J,H,E,J,H,E,T,J,T,J,E,H,J,T,E,H,E"
+    dice = [1,2,4,1,7,8,5,11,10,9,2,6,1,3,7,9,5,11,10,12,2,3,5,6,7,8,5,11,10,12]
+    
+    game = BusinessGame(cells, dice, n_players=3)
+    game.run_game()
+    game.display_winner()
